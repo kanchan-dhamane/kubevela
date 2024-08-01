9d04ce51f3865712f00cf6ade244f898090829f4
kanchan-dhamane <74534570+kanchan-dhamane@users.noreply.github.com>
2024-08-01 13:39:31 +0530

    implements a conversion

--- apis/core.oam.dev/v1beta1/componentdefinition_conversion.go
+++ apis/core.oam.dev/v1beta1/componentdefinition_conversion.go
@@ -1,17 +1,51 @@
 package v1beta1
 
 import (
-	"fmt"
-
 	"sigs.k8s.io/controller-runtime/pkg/conversion"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+
+	"github.com/oam-dev/kubevela/apis/core.oam.dev/v1beta2"
 )
 
+var ComponentDefinitionlog = logf.Log.WithName("ComponentDefinition-resource")
+
+// convert v1lbeta1 to v1beta2
 func (src *ComponentDefinition) ConvertTo(dstRaw conversion.Hub) error {
-	fmt.Println("ConvertTo to v1beta2---")
+	ComponentDefinitionlog.Info("ConvertTo to v1beta2--- ----------------")
+	dst := dstRaw.(*v1beta2.ComponentDefinition)
+	version := "1.6.7"
+	dst.ObjectMeta = src.ObjectMeta
+	componentDefinitionSpecTest := &v1beta2.ComponentDefinitionSpecTest{}
+	componentDefinitionSpecTest.Version = version
+	componentDefinitionSpecTest.Workload = src.Spec.Workload
+	componentDefinitionSpecTest.ChildResourceKinds = src.Spec.ChildResourceKinds
+	componentDefinitionSpecTest.RevisionLabel = src.Spec.RevisionLabel
+	componentDefinitionSpecTest.PodSpecPath = src.Spec.PodSpecPath
+	componentDefinitionSpecTest.Status = src.Spec.Status
+	componentDefinitionSpecTest.Schematic = src.Spec.Schematic
+	componentDefinitionSpecTest.Extension = src.Spec.Extension
+	dst.Spec.Versions = []v1beta2.ComponentDefinitionSpecTest{*componentDefinitionSpecTest}
+
 	return nil
 }
 
-func (src *ComponentDefinition) ConvertFrom(dstRaw conversion.Hub) error {
-	fmt.Println("ConvertFrom to v1beta2---")
+// convert v1beta2 to v1beta1
+func (dst *ComponentDefinition) ConvertFrom(srcRaw conversion.Hub) error {
+	ComponentDefinitionlog.Info("ConvertFrom to v1beta2--- ------------------")
+	src := srcRaw.(*v1beta2.ComponentDefinition)
+
+	if len(src.Spec.Versions) >= 1 {
+		ComponentDefinitionlog.Info(src.Spec.Versions[0].Version)
+	}
+
+	componentDefinitionSpec := &ComponentDefinitionSpec{}
+	componentDefinitionSpec.Workload = src.Spec.Versions[0].Workload
+	componentDefinitionSpec.ChildResourceKinds = src.Spec.Versions[0].ChildResourceKinds
+	componentDefinitionSpec.RevisionLabel = src.Spec.Versions[0].RevisionLabel
+	componentDefinitionSpec.Status = src.Spec.Versions[0].Status
+	componentDefinitionSpec.Schematic = src.Spec.Versions[0].Schematic
+	componentDefinitionSpec.Extension = src.Spec.Versions[0].Extension
+	dst.Spec = *componentDefinitionSpec
+	dst.ObjectMeta = src.ObjectMeta
 	return nil
 }

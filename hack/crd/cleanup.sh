b4f6d70ed91cdc5e98d5f0817a678a8dd24f9e1c
bugbounce <35751394+bugbounce@users.noreply.github.com>
2024-08-09 21:23:26 +0530

    Fix conversion webhook path for COmponentDefinition

--- hack/crd/cleanup.sh
+++ hack/crd/cleanup.sh
@@ -6,8 +6,8 @@ SCRIPT_DIR=$(dirname "$0")
 pushd "$SCRIPT_DIR"
 
 TEMPLATE_DIR="../../config/crd/base"
-mv ${TEMPLATE_DIR}/apiextensions.k8s.io_v1_customresourcedefinition_componentdefinitions.core.oam.dev.yaml ${TEMPLATE_DIR}/core.oam.dev_componentdefinitions.yaml
-mv ${TEMPLATE_DIR}/apiextensions.k8s.io_v1_customresourcedefinition_applications.core.oam.dev.yaml ${TEMPLATE_DIR}/core.oam.dev_applications.yaml
+mv ${TEMPLATE_DIR}/*componentdefinitions.core.oam.dev.yaml ${TEMPLATE_DIR}/core.oam.dev_componentdefinitions.yaml
+mv ${TEMPLATE_DIR}/*applications.core.oam.dev.yaml ${TEMPLATE_DIR}/core.oam.dev_applications.yaml
 
 echo "clean up unused fields of CRDs"
 
